def485d485338fb9855603e2d1cd6adc108fb881
kim kyeong soo <with317@gmail.com>
2021-03-21 00:59:58 +0900

    -1

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 //export const SERVER_URI = "http://localhost:8000/graphql";
-export const SERVER_URI = "http://13.125.216.2:8000/graphql";
+export const SERVER_URI = "http://15.165.205.153:8000/graphql";
